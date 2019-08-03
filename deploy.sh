e2416e7068398ad76cf81d493955c70b965a016e
anh.vuong <anh.vuong@tbwa.com>
2019-08-03 23:04:45 +0700

    add file deploy code

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com/vuongducanh/vuex.git master:gh-pages
+git push -f https://github.com/vuongducanh/vuex.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/todo-app/'
+  publicPath: '/vuex/'
 }
